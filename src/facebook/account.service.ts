d45c91f699b191e5ffa426b3113e8d87166cc256
HM <hieumdd@gmail.com>
2023-03-16 23:23:12 +0700

    feat: Update taskService

--- src/facebook/account.service.ts
+++ src/facebook/account.service.ts
@@ -10,18 +10,21 @@ export const ACCOUNTS = [
 ];
 
 export type TaskOptions = {
-    pipeline: keyof typeof pipelines;
     start?: string;
     end?: string;
 };
 
-export const taskService = ({ pipeline, start, end }: TaskOptions) => {
-    const data = Object.values(ACCOUNTS).map((accountId) => ({
-        accountId: String(accountId),
-        start,
-        end,
-        pipeline,
-    }));
+export const taskService = ({ start, end }: TaskOptions) => {
+    const data = Object.values(ACCOUNTS).flatMap((accountId) => {
+        return [pipelines.ADS_INSIGHTS, pipelines.CAMPAIGNS_PUBLISHER_PLATFORM_INSIGHTS].map(
+            (pipeline) => ({
+                accountId: String(accountId),
+                start,
+                end,
+                pipeline,
+            }),
+        );
+    });
 
     return createTasks(data, (task) => task.accountId);
 };
