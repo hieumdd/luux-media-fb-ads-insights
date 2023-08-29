564832ef500978577d27d42f6e4412a537d48a55
HM <hieumdd@gmail.com>
2023-08-29 14:59:54 +0700

    feat: Adjust dimension.service

--- src/facebook/dimension.service.ts
+++ src/facebook/dimension.service.ts
@@ -1,4 +1,3 @@
-import dayjs from '../dayjs';
 import { getClient, getExtractStream } from './api.service';
 import { PipelineOptions } from '../pipeline/pipeline.request.dto';
 
@@ -8,25 +7,13 @@ type GetDimensionsConfig = {
 };
 
 export const getDimensionStream = ({ endpoint, fields }: GetDimensionsConfig) => {
-    return async ({ accountId, start, end }: PipelineOptions) => {
+    return async ({ accountId }: PipelineOptions) => {
         const client = await getClient();
 
         return getExtractStream(client, (after) => ({
             method: 'GET',
             url: `/act_${accountId}/${endpoint}`,
             params: { fields, limit: 250, after },
-            filtering: [
-                {
-                    field: 'ad.updated_time',
-                    operator: 'GREATER_THAN_OR_EQUAL',
-                    value: dayjs.utc(start).unix(),
-                },
-                {
-                    field: 'ad.updated_time',
-                    operator: 'LESS_THAN_OR_EQUAL',
-                    value: dayjs.utc(end).unix(),
-                },
-            ],
         }));
     };
 };
