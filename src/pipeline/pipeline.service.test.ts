083c3430d9e1aeb6ca04e66b4000b6a4eb114dab
HM <hieumdd@gmail.com>
2023-08-29 15:12:02 +0700

    misc: Update test

--- src/pipeline/pipeline.service.test.ts
+++ src/pipeline/pipeline.service.test.ts
@@ -1,11 +1,15 @@
 import * as pipelines from './pipeline.const';
-import { createPipelineTasks, runPipeline } from './pipeline.service';
+import {
+    createAdsPipelineTasks,
+    createInsightsPipelineTasks,
+    runPipeline,
+} from './pipeline.service';
 
 it('pipeline', async () => {
-    return runPipeline(pipelines.ADS_INSIGHTS, {
-        accountId: '1353175741501928',
+    return runPipeline(pipelines.ADS, {
+        accountId: '193539783445588',
         start: '2023-01-01',
-        end: '2023-08-01',
+        end: '2023-09-01',
     })
         .then((results) => {
             expect(results).toBeDefined();
@@ -16,8 +20,20 @@ it('pipeline', async () => {
         });
 });
 
-it('create-tasks', async () => {
-    return createPipelineTasks({
+it('create-tasks-insights', async () => {
+    return createInsightsPipelineTasks({
+        start: '2023-01-01',
+        end: '2023-03-01',
+    })
+        .then((result) => expect(result).toBeDefined())
+        .catch((error) => {
+            console.error({ error });
+            return Promise.reject(error);
+        });
+});
+
+it('create-tasks-ads', async () => {
+    return createAdsPipelineTasks({
         start: '2023-01-01',
         end: '2023-03-01',
     })
