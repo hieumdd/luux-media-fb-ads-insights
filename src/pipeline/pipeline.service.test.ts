a8823b805ddbf8dfe550f336c857918424420351
HM <hieumdd@gmail.com>
2023-12-13 20:43:12 +0700

    ci: Add more tests

--- src/pipeline/pipeline.service.test.ts
+++ src/pipeline/pipeline.service.test.ts
@@ -1,15 +1,36 @@
 import * as pipelines from './pipeline.const';
 import {
-    createAdsPipelineTasks,
+    createCustomPipelineTasks,
     createInsightsPipelineTasks,
     runPipeline,
 } from './pipeline.service';
 
-it('pipeline', async () => {
-    return runPipeline(pipelines.ADS, {
-        accountId: '193539783445588',
-        start: '2023-01-01',
-        end: '2023-09-01',
+describe('pipeline', () => {
+    it.each([
+        pipelines.ADS_PUBLISHER_PLATFORM_INSIGHTS,
+        pipelines.CAMPAIGNS_COUNTRY_INSIGHTS,
+        pipelines.CAMPAIGNS_DEVICE_PLATFORM_POSITION_INSIGHTS,
+    ])('$.name', async (pipeline) => {
+        return runPipeline(pipeline, {
+            accountId: '1035385077897706',
+            start: '2023-07-01',
+            end: '2023-12-01',
+        })
+            .then((results) => {
+                expect(results).toBeDefined();
+            })
+            .catch((error) => {
+                console.error({ error });
+                return Promise.reject(error);
+            });
+    });
+});
+
+it('pispeline', async () => {
+    return runPipeline(pipelines.ADS_PUBLISHER_PLATFORM_INSIGHTS, {
+        accountId: '406026759976583',
+        start: '2023-06-01',
+        end: '2023-08-01',
     })
         .then((results) => {
             expect(results).toBeDefined();
@@ -22,8 +43,8 @@ it('pipeline', async () => {
 
 it('create-tasks-insights', async () => {
     return createInsightsPipelineTasks({
-        start: '2023-01-01',
-        end: '2023-03-01',
+        start: '2023-08-28',
+        end: '2023-09-04',
     })
         .then((result) => expect(result).toBeDefined())
         .catch((error) => {
@@ -33,9 +54,9 @@ it('create-tasks-insights', async () => {
 });
 
 it('create-tasks-ads', async () => {
-    return createAdsPipelineTasks({
-        start: '2023-01-01',
-        end: '2023-03-01',
+    return createCustomPipelineTasks({
+        start: '2023-08-28',
+        end: '2023-09-04',
     })
         .then((result) => expect(result).toBeDefined())
         .catch((error) => {
