94ed3e00ca16928e269175f98139ae24ebdda555
HM <hieumdd@gmail.com>
2024-03-17 21:42:32 +0700

    feat: Update api version

--- src/facebook/api.service.ts
+++ src/facebook/api.service.ts
@@ -6,23 +6,22 @@ import { getLogger } from '../logging.service';
 
 const logger = getLogger(__filename);
 
-export const getClient = async () => {
-    const accessToken = await axios
+const getSecret = async (name: string) => {
+    return await axios
         .request<{ value: { raw: string } }>({
             method: 'GET',
             url: 'https://api.doppler.com/v3/configs/config/secret',
-            params: { project: 'facebook', config: 'master', name: 'USER_ACCESS_TOKEN' },
+            params: { project: 'facebook', config: 'master', name },
             auth: { username: process.env.DOPPLER_TOKEN ?? '', password: '' },
         })
         .then(({ data }) => data.value.raw);
+};
 
-    const apiVersion = await axios
-        .request({
-            method: 'GET',
-            url: 'https://graph.facebook.com/me',
-            params: { access_token: accessToken },
-        })
-        .then((response) => <string>response.headers['facebook-api-version']);
+export const getClient = async () => {
+    const [apiVersion, accessToken] = await Promise.all([
+        getSecret('API_VERSION'),
+        getSecret('USER_ACCESS_TOKEN'),
+    ]);
 
     const client = axios.create({
         baseURL: `https://graph.facebook.com/${apiVersion}`,
